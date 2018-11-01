e92d8606ba23b6235495a08b2f8eecddcad29542
mahhov <mahhov1@gmail.com>
2018-11-01 14:59:26 -0700

    utility $ & $$ for XElement

--- src/xelements/XElement.js
+++ src/xelements/XElement.js
@@ -4,6 +4,16 @@ class XElement extends HTMLElement {
 		this.attachShadow({mode: 'open'});
 		this.shadowRoot.innerHTML = htmlString;
 	}
+
+	$(query) {
+		return this.shadowRoot.querySelector(query);
+	}
+
+	$$(query) {
+		return this.shadowRoot.querySelectorAll(query);
+	}
 }
 
 module.exports = XElement;
+
+// todo use template https://developers.google.com/web/fundamentals/web-components/examples/howto-checkbox ?
