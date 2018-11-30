d1a7b1b50e59f5f9b9b9cdb1eb9a8192dd128ade
mahhov <mahhov1@gmail.com>
2018-11-30 06:03:05 -0800

    wip playlist panel and list

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 const styleSharing = require('shadow-dom-style-sharing');
 require('./xelements/import');
+
 const SongStorage = require('./storage/SongStorage');
-
 const songStorage = new SongStorage();
-
 // songStorage.writeSong({id: 3, title: 'title', audioData: 'xcxcxxxcx'})
 // songStorage.getAllSongs().then(x => console.log(x))
 
@@ -22,6 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
 	$('x-player').addEventListener('end', () => {
 		console.log('ended caught by main.js');
 	});
+
+	// $('x-playlist-panel').list = [
+	// 	{title: 'x-playlist-panel', count: 3, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
+	// 	{title: 'my playlist name', count: 300, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
+	// 	{title: 'new', count: 1400, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
+	// ];
 
 	styleSharing.process(document);
 
