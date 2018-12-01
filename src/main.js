4c6dfef38132ecddb3d264c503034c54fde920e7
mahhov <mahhov1@gmail.com>
2018-11-30 19:51:52 -0800

    temp

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 const styleSharing = require('shadow-dom-style-sharing');
 require('./xelements/import');
 
-const SongStorage = require('./storage/SongStorage');
-const songStorage = new SongStorage();
+// const SongStorage = require('./storage/SongStorage');
+// const songStorage = new SongStorage();
 // songStorage.writeSong({id: 3, title: 'title', audioData: 'xcxcxxxcx'})
 // songStorage.getAllSongs().then(x => console.log(x))
 
@@ -22,24 +22,5 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.log('ended caught by main.js');
 	});
 
-	// $('x-playlist-panel').list = [
-	// 	{title: 'x-playlist-panel', count: 3, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
-	// 	{title: 'my playlist name', count: 300, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
-	// 	{title: 'new', count: 1400, id: 'OLAK5uy_mt1gUnCahoe2g5rYOCCxLU_pMxBxcSbPw'},
-	// ];
-
 	styleSharing.process(document);
-
-// 	let installEvent;
-// 	window.addEventListener('beforeinstallprompt', event => {
-// 		$('install-button').hidden = false;
-// 		installEvent = event;
-//
-// 	});
-// 	$('install-button').addEventListener('click', event => installEvent.prompt());
-//
-// 	$('directory-selector').addEventListener('change', (e) => {
-// 		let files = [...$('directory-selector').files];
-//
-// 	});
 });
