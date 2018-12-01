05e7bd0d6b8fdcc7543276036bbc73e0fb008296
mahhov <mahhov1@gmail.com>
2018-11-30 19:37:09 -0800

    toggle to sync with host attribute checked

--- src/xelements/toggle/toggle.js
+++ src/xelements/toggle/toggle.js
@@ -2,6 +2,10 @@ const template = require('fs').readFileSync(`${__dirname}/toggle.html`, 'utf8');
 const XElement = require('../XElement');
 
 customElements.define('x-toggle', class extends XElement {
+	static get observedAttributes() {
+		return ['checked'];
+	}
+
 	constructor() {
 		super(template);
 	}
@@ -10,7 +14,22 @@ customElements.define('x-toggle', class extends XElement {
 		this.$('#checkbox').addEventListener('change', () => this.onChange_())
 	}
 
+	get checked() {
+		return this.hasAttribute('checked');
+	}
+
+	set checked(value) {
+		if (value)
+			this.setAttribute('checked', '');
+		else
+			this.removeAttribute('checked');
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.$('#checkbox').checked = newValue === '';
+	}
+
 	onChange_() {
-		this.dispatchEvent(new CustomEvent('change', {detail: this.$('#checkbox').checked}));
+		this.dispatchEvent(new CustomEvent('change', {detail: this.checked}));
 	}
 });
