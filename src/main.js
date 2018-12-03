c14fff9f3c554ed2e73f71da3afa1719272767f6
mahhov <mahhov1@gmail.com>
2018-12-03 05:50:22 -0800

    frames hidindg and showing based on selection

--- src/main.js
+++ src/main.js
@@ -1,26 +1,9 @@
 const styleSharing = require('shadow-dom-style-sharing');
 require('./xelements/import');
 
-// const SongStorage = require('./storage/SongStorage');
-// const songStorage = new SongStorage();
-// songStorage.writeSong({id: 3, title: 'title', audioData: 'xcxcxxxcx'})
-// songStorage.getAllSongs().then(x => console.log(x))
-
 document.addEventListener('DOMContentLoaded', () => {
 	let $ = query =>
 		document.querySelector(query);
-
-	$('#song-list').list = [
-		'flamingo',
-		'elemphant',
-		'mouse',
-	];
-
-	$('#song-list').addEventListener('song-select', ({detail}) => console.log(detail));
-
-	$('x-player').addEventListener('end', () => {
-		console.log('ended caught by main.js');
-	});
 
 	styleSharing.process(document);
 });
--- src/xelements/frame/frame.js
+++ src/xelements/frame/frame.js
@@ -7,14 +7,24 @@ customElements.define('x-frame', class Frame extends XElement {
 		}
 
 		connectedCallback() {
-			this.$('#player').addEventListener('change', () => this.onSelect_(0));
-			this.$('#list').addEventListener('change', () => this.onSelect_(1));
-			this.$('#downloader').addEventListener('change', () => this.onSelect_(2));
+			this.frames = [
+				this.$('#player-frame'),
+				this.$('#list-frame'),
+				this.$('#downloader-frame')];
+
+			this.selects = [
+				this.$('#player-select'),
+				this.$('#list-select'),
+				this.$('#downloader-select')];
+
+			this.selects.forEach((select, i) => select.addEventListener('change', () => this.onSelect_(i)))
+
+			this.onSelect_(0);
 		}
 
 		onSelect_(index) {
-			[this.$('#player'), this.$('#list'), this.$('#downloader')]
-				.forEach((item, i) => item.checked = i === index);
+			this.selects.forEach((item, i) => item.checked = i === index);
+			this.frames.forEach((item, i) => item.classList.toggle('hidden-frame', i !== index));
 		}
 	}
 );
