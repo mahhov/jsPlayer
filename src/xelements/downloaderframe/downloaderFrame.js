3dd7f4f9eb164a1a0850f706895ebc76ca3c40c4
mahhov <mahhov1@gmail.com>
2018-12-24 09:33:02 -0800

    downloaderFrame

---
+++ src/xelements/downloaderframe/downloaderFrame.js
@@ -0,0 +1,58 @@
+const template = require('fs').readFileSync(`${__dirname}/downloaderFrame.html`, 'utf8');
+const XElement = require('../XElement');
+const styleSharing = require('shadow-dom-style-sharing');
+const dwytpl = require('dwytpl');
+const DW_PATH = './downloads';
+
+customElements.define('x-downloader-frame', class DownloaderFrame extends XElement {
+	constructor() {
+		super(template);
+
+		this.$('#add-playlist').addEventListener('click', () => this.onAddPlaylist_());
+		this.$('#refresh-all').addEventListener('click', () => this.onRefreshAll_());
+
+		this.addPlaylistPanel_('PLameShrvoeYfp54xeNPK1fGxd2a7IzqU2');
+		this.addPlaylistPanel_('PLameShrvoeYfzOWuBX2bbER0LXD9EuxGx');
+	}
+
+	onAddPlaylist_() {
+		this.addPlaylistPanel_(this.$('#playlist-id').value);
+	}
+
+	addPlaylistPanel_(playlistId) {
+		let playlistPanel = document.createElement('x-playlist-panel');
+		styleSharing.process(playlistPanel);
+		playlistPanel.playlistId = playlistId;
+
+		playlistPanel.addEventListener('download', ({detail: tracker}) => {
+			this.connectTracker(tracker);
+			this.$$('#playlist-panels-list x-playlist-panel').forEach(playlistPanelIter => {
+				
+				if (playlistPanelIter !== playlistPanel)
+					playlistPanelIter.stopDownload();
+			});
+		});
+
+		this.$('#playlist-panels-list').appendChild(playlistPanel);
+	}
+
+	onRefreshAll_() {
+		this.$$('#playlist-panels-list x-playlist-panel').forEach(playlistPanel =>
+			playlistPanel.refresh());
+	}
+
+	connectTracker(tracker) {
+		if (this.connectedTracker_) {
+			tracker.title.disconnect();
+			tracker.summary.disconnect();
+			tracker.progerss.disconnect();
+			tracker.messages.disconnect();
+		}
+		this.connectedTracker_ = tracker;
+
+		tracker.title.each(lines => this.$('#title').lines = lines);
+		tracker.summary.each(lines => this.$('#summary').lines = lines);
+		tracker.progerss.each(lines => this.$('#progress').lines = lines);
+		tracker.messages.each(lines => this.$('#messages').lines = lines);
+	}
+});
