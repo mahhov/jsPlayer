91a6925fb12011269b9e5b85ee8b73320d2dbafe
mahhov <mahhov1@gmail.com>
2019-01-07 15:59:40 -0800

    get downloads song list

--- src/storage/SongStorage.js
+++ src/storage/SongStorage.js
@@ -1,24 +1,7 @@
-const requestp = require('./requestPromise');
+const fs = require('fs').promises;
+const DOWNLOAD_DIR = 'downloads';
 
-class SongStorage {
-	constructor() {
-		this.open = requestp(indexedDB.open('songsDb'), 'open db');
-		this.open.upgradep.then(db => db.createObjectStore('songs', {keyPath: 'id'}));
-	}
+let getSongList = () =>
+	fs.readdir(DOWNLOAD_DIR);
 
-	writeSong(song) {
-		this.open.successp.then(db => {
-			let songStore = db.transaction('songs', 'readwrite').objectStore('songs');
-			requestp(songStore.put(song), 'write song');
-		});
-	}
-
-	getAllSongs() {
-		return this.open.successp.then(db => {
-			let songsStore = db.transaction('songs').objectStore('songs');
-			return requestp(songsStore.getAll(), 'get all songs').successp;
-		});
-	}
-}
-
-module.exports = SongStorage;
+module.exports = {getSongList};
