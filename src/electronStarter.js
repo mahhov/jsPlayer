1528991dc51d22204d577c75ed3fe9eacd6d69d7
mahhov <mahhov1@gmail.com>
2019-01-27 11:24:20 -0800

    auto start play on startup

--- src/electronStarter.js
+++ src/electronStarter.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const {app, BrowserWindow} = require('electron');
 
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
 
 app.on('ready', () => {
 	let window = new BrowserWindow({width: 1800, height: 1000});
