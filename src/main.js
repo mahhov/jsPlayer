8e516d0bc977ec80337d04f386a6444e7233e964
mahhov <mahhov1@gmail.com>
2019-01-27 13:04:25 -0800

    todo list

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	styleSharing.process(document);
 });
+
+// todo
+// persistent playlist list, shuffle, volume
+// notifications on download and song change
+// keyboard shortcuts, global and local, skip/rewind 5 seconds, skip/rewind song, pause play
+// ui to have less refresh buttons, automatically refresh player and list on new download
+// way to delete songs from list, and refresh download frame status and player
+// ui to look good
+// select player song from clicking on song from list
