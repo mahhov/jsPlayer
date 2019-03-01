b822a82e5ea6255fbd4e9419c713b83883e7e070
mahhov <mahhov1@gmail.com>
2019-03-01 06:59:29 -0800

    audio visualizere, introduce low smooth bars

--- src/xelements/audiovisualizer/audioVisualizer.js
+++ src/xelements/audiovisualizer/audioVisualizer.js
@@ -9,14 +9,18 @@ class Visualizer {
 		this.height_ = height;
 		this.canvasCtx_ = canvasCtx;
 		this.smoothValues_ = Array(barCount).fill(0);
+		this.lowSmoothValues_ = Array(barCount).fill(0);
 
 		this.clearColor = [0, 0, 0];
 		this.leftColor = [255, 0, 0];
 		this.rightColor = [0, 0, 255];
 		this.leftSmoothColor = [255, 150, 100];
 		this.rightSmoothColor = [100, 150, 255];
+		this.leftLowSmoothColor = [200, 0, 0];
+		this.rightLowSmoothColor = [0, 0, 200];
 		this.blurColor = [200, 100, 200];
 		this.smoothWeight = .98;
+		this.lowSmoothValueMult = .8;
 		this.blur = .05;
 
 		this.xMargin_ = this.width_ * .03;
@@ -40,6 +44,10 @@ class Visualizer {
 			.map((value, i) => value * this.smoothWeight + values[i] * (1 - this.smoothWeight))
 			.map((value, i) => Math.max(value, values[i]));
 
+		this.lowSmoothValues_ = this.lowSmoothValues_
+			.map((value, i) => value * this.smoothWeight + values[i] * (1 - this.smoothWeight))
+			.map((value, i) => Math.min(value, values[i]));
+
 		this.canvasCtx_.fillStyle = Visualizer.rgb(...this.clearColor);
 		this.canvasCtx_.fillRect(0, 0, this.width_, this.height_);
 
@@ -49,6 +57,7 @@ class Visualizer {
 			let left = i * this.barSpace_ + this.xMargin_ + (this.barSpace_ - this.barWidth_) / 2;
 			let top = this.height_ - barHeight - this.yMargin_;
 			let smoothHeight = (this.smoothValues_[i] - value) * this.maxBarHeight_;
+			let lowSmoothHeight = this.lowSmoothValues_[i] * this.maxBarHeight_ * this.lowSmoothValueMult;
 			let blurHeight = barHeight * this.blur;
 
 			if (smoothHeight > 0) {
@@ -58,6 +67,9 @@ class Visualizer {
 
 			this.canvasCtx_.fillStyle = Visualizer.averageColor(this.leftColor, this.rightColor, gradiant);
 			this.canvasCtx_.fillRect(left, top, this.barWidth_, barHeight);
+
+			this.canvasCtx_.fillStyle = Visualizer.averageColor(this.leftLowSmoothColor, this.rightLowSmoothColor, gradiant);
+			this.canvasCtx_.fillRect(left, top + barHeight - lowSmoothHeight, this.barWidth_, lowSmoothHeight);
 
 			this.canvasCtx_.fillStyle = Visualizer.rgb(...this.blurColor);
 			this.canvasCtx_.fillRect(left, top - blurHeight, this.barWidth_, blurHeight);
