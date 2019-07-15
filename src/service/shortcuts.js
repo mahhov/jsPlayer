398da026962296f201d4f3d4e294321ecb7a6293
manuk <mahhov1@gmail.com>
2019-07-15 16:37:37 -0700

    blur input accepting input boxes on listframe & downoad frame on escape press

--- src/service/shortcuts.js
+++ src/service/shortcuts.js
@@ -7,6 +7,8 @@ let addListenerGlobalNext = handler => ipc.on('shortcutNext', handler);
 let addListenerGlobalPause = handler => ipc.on('shortcutPause', handler);
 
 let addListenerKeydown = handler => document.addEventListener('keydown', e => {
+	if (e.path[0].hasAttribute('consume-key') && e.key === 'Escape')
+		e.path[0].blur();
 	if (!e.repeat && !e.path[0].hasAttribute('consume-key'))
 		handler(e);
 });
