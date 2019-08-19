c3ad3df8bf33ffa3ccd6b9917b62caf16ae6bd8b
manuk <mahhov1@gmail.com>
2019-08-19 06:43:04 -0700

    clear button for explorer frame

--- src/xelements/explorerframe/explorerFrame.js
+++ src/xelements/explorerframe/explorerFrame.js
@@ -10,10 +10,24 @@ customElements.define('x-explorer-frame', class DownloaderFrame extends XElement
 
 	connectedCallback() {
 		this.$('#search-button').addEventListener('click', () => this.query_());
+		this.$('#clear-button').addEventListener('click', () => this.clear_());
+
+		this.clear_();
+	}
+
+	query_() {
+		this.search_.query(this.$('#search').value);
+	}
+
+	clear_() {
+		if (this.syncher_) {
+			this.syncher_.stopDownload();
+			this.syncher_.tracker.summary.disconnect();
+		}
 
 		this.search_ = new dwytpl.Search(this.playlistId);
 		this.syncher_ = new dwytpl.Syncher(this.search_);
-		this.syncher_.setDownloadDir(storage.tempDownloadDir);
+		this.syncher_.setDownloadDir(storage.explorerDownloadDir);
 		this.syncher_.download();
 
 		this.syncher_.tracker.summary.each(summaryText => {
@@ -21,6 +35,7 @@ customElements.define('x-explorer-frame', class DownloaderFrame extends XElement
 			this.$('#summary-line-two').textContent = summaryText[1];
 		});
 
+		XElement.clearChildren(this.$('#list'));
 		this.search_.getVideos().each(video => {
 			let name = document.createElement('div');
 			name.textContent = video.getName_();
@@ -32,18 +47,14 @@ customElements.define('x-explorer-frame', class DownloaderFrame extends XElement
 			this.$('#list').appendChild(container);
 		});
 	}
-
-	query_() {
-		this.search_.query(this.$('#search').value);
-	}
 });
 
 // todo
 // styling
 // keyboard shortcuts
-// clear
 // click to play
 // auto play
 // add
 // remove
 // indicate which already added
+// add
