00f205bdb62316beeb7877701490b521b9cf0c98
manuk <mahhov1@gmail.com>
2019-08-26 06:03:08 -0700

    add google auth class to handle oauth2

---
+++ src/service/GoogleAuth.js
@@ -0,0 +1,107 @@
+const https = require('https');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+class GoogleAuth {
+	/*
+		Certificate:
+		{
+		  "installed": {
+		    "client_id": "663996541334-4rdijihf9s0hub89lourvsbhg23khjsm.apps.googleusercontent.com",
+		    "project_id": "js-player-250603",
+		    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+		    "token_uri": "https://oauth2.googleapis.com/token",
+		    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+		    "client_secret": "zVBWziJYqByN87puEm5OOtpn",
+		    "redirect_uris": [
+		      "urn:ietf:wg:oauth:2.0:oob",
+		      "http://localhost"
+		    ]
+		  }
+		}
+		Tokens response:
+		{
+			access_token: 'xyz',
+			expires_in: 3600,
+			refresh_token: 'abc',
+			scope: 'https://www.googleapis.com/auth/youtube',
+			token_type: 'Bearer'
+		}
+	*/
+	constructor(credentialsPath, tokensPath, scope, redirectUri) {
+		this.credentials_ = GoogleAuth.readJson_(credentialsPath, true).installed;
+		this.tokens_ = GoogleAuth.readJson_(tokensPath);
+		this.tokensPath_ = tokensPath;
+		this.scope_ = scope;
+		this.redirectUri_ = redirectUri;
+	}
+
+	static readJson_(jsonPath, reportErrors) {
+		try {
+			return JSON.parse(fs.readFileSync(path.resolve(jsonPath), 'utf-8'));
+		} catch (e) {
+			if (reportErrors)
+				throw e;
+		}
+	}
+
+	openConsentScreen_(endpoint) {
+		/* override */
+		// return Promise<string redirectUrl>
+	}
+
+	async getToken() {
+		if (this.token_)
+			return this.token_;
+		if (this.tokens_)
+			return this.refreshToken_(this.tokens_.refresh_token);
+		let code = await this.requestCode_();
+		this.tokens_ = await this.requestToken_(code);
+		fs.writeFile(this.tokensPath_, JSON.stringify(this.tokens_, null, 2), err => {
+			if (err)
+				throw err;
+		});
+		return this.token_ = this.tokens_.access_token;
+	}
+
+	getRefreshedToken() {
+		this.token_ = null;
+		return this.getToken();
+	}
+
+	async requestCode_() {
+		let consentEndpoint =
+			`${this.credentials_.auth_uri}?client_id=${this.credentials_.client_id}&redirect_uri=${this.redirectUri_}&response_type=code&scope=${this.scope_}`
+				.replace(/&/g, '^&');
+		let redirectUrl = await this.openConsentScreen_(consentEndpoint);
+		return url.parse(redirectUrl, true).query.code;
+	}
+
+	async requestToken_(code) {
+		return new Promise(resolve =>
+			https.request(this.credentials_.token_uri, {method: 'POST'}, res =>
+				res.on('data', data => resolve(JSON.parse(data.toString())))
+			).end(JSON.stringify({
+				code: code,
+				client_id: this.credentials_.client_id,
+				client_secret: this.credentials_.client_secret,
+				redirect_uri: this.redirectUri_,
+				grant_type: 'authorization_code',
+			})));
+	}
+
+	async refreshToken_(refreshToken) {
+		return new Promise(resolve =>
+			https.request(this.credentials_.token_uri, {method: 'POST'}, res =>
+				res.on('data', data => resolve(JSON.parse(data.toString())))
+			).end(JSON.stringify({
+				refresh_token: refreshToken,
+				client_id: this.credentials_.client_id,
+				client_secret: this.credentials_.client_secret,
+				grant_type: 'refresh_token',
+			})));
+	}
+}
+
+module.exports = GoogleAuth;
