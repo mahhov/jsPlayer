1df01a4b3bf6cf1b4dfe4746aaf62ecd1889f2fc
manuk <mahhov1@gmail.com>
2019-08-31 16:40:44 -0700

    convert x-downloading-song-line to xelement2, 47% code reduction and added support for status prop default values and converted status prop from boolean/null enum to plain strings enum

--- src/xelements/downloadingsongline/downloadingSongLine.js
+++ src/xelements/downloadingsongline/downloadingSongLine.js
@@ -1,105 +1,53 @@
 const template = require('fs').readFileSync(`${__dirname}/downloadingSongLine.html`, 'utf8');
-const XElement = require('../XElement');
+const XElement = require('../XElement2');
 
 customElements.define('x-downloading-song-line', class extends XElement {
-		static get observedAttributes() {
-			return ['title', 'status', 'playlist-status', 'download-status', 'play-status'];
+		static get attributeTypes() {
+			return {
+				title: false,
+				status: false,
+				playlistStatus: false,
+				downloadStatus: false,
+				playStatus: false,
+			};
 		}
 
-		constructor() {
-			super(template);
+		static get htmlTemplate() {
+			return template;
 		}
 
 		connectedCallback() {
-			if (!this.hasAttribute('title'))
-				this.setAttribute('title', '');
-			if (!this.hasAttribute('status'))
-				this.setAttribute('status', '');
-			// todo initialize to null
-			// if (!this.hasAttribute('playlist-status'))
-			// 	this.removeAttribute('playlist-status');
-			// if (!this.hasAttribute('download-status'))
-			// 	this.removeAttribute('download-status');
-			// if (!this.hasAttribute('play-status'))
-			// 	this.removeAttribute('play-status');
-
+			this.playlistStatus = 'undetermined';
+			this.downloadStatus = 'undetermined';
+			this.playStatus = 'undetermined';
 			this.$('#container').addEventListener('click', () => this.emitSelect_());
 		}
 
-		get title() {
-			return this.getAttribute('title');
-		}
-
 		set title(value) {
-			this.setAttribute('title', value);
-		}
-
-		get status() {
-			return this.getAttribute('status');
+			this.$(`#title`).textContent = value;
 		}
 
 		set status(value) {
-			this.setAttribute('status', value);
-		}
-
-		get playlistStatus() {
-			return this.getAttribute('playlist-status'); // todo return true, false, null rather than 'true', 'false', null
+			this.$(`#status`).textContent = value;
 		}
 
 		set playlistStatus(value) {
-			if (value === null)
-				this.removeAttribute('playlist-status');
-			else
-				this.setAttribute('playlist-status', value);
-		}
-
-		get downloadStatus() {
-			return this.getAttribute('download-status');
+			this.$('#container').classList.toggle('playlist-added', value === 'true');
+			this.$('#container').classList.toggle('playlist-not-added', value === 'false');
+			this.$('#container').classList.toggle('playlist-undetermined', value === 'undetermined');
 		}
 
 		set downloadStatus(value) {
-			if (value === null)
-				this.removeAttribute('download-status');
-			else
-				this.setAttribute('download-status', value);
-		}
-
-		get playStatus() {
-			return this.getAttribute('play-status');
+			this.$('#container').classList.toggle('download-success', value === 'true');
+			this.$('#container').classList.toggle('download-fail', value === 'false');
+			this.$('#container').classList.toggle('download-undetermined', value === 'undetermined');
 		}
 
 		set playStatus(value) {
-			if (value === null)
-				this.removeAttribute('play-status');
-			else
-				this.setAttribute('play-status', value);
-		}
-
-		attributeChangedCallback(name, oldValue, newValue) {
-			switch (name) {
-				case 'title':
-					this.$(`#title`).textContent = newValue;
-					break;
-				case 'status':
-					this.$(`#status`).textContent = newValue;
-					break;
-				case 'playlist-status':
-					this.$('#container').classList.toggle('playlist-added', newValue === 'true');
-					this.$('#container').classList.toggle('playlist-not-added', newValue === 'false');
-					this.$('#container').classList.toggle('playlist-undetermined', newValue === null);
-					break;
-				case 'download-status':
-					this.$('#container').classList.toggle('download-success', newValue === 'true');
-					this.$('#container').classList.toggle('download-fail', newValue === 'false');
-					this.$('#container').classList.toggle('download-undetermined', newValue === null);
-					break;
-				case 'play-status':
-					this.$('#container').classList.toggle('selected', newValue === 'true'); // for element.css
-					this.$('#container').classList.toggle('play-playing', newValue === 'true'); // for consistency
-					this.$('#container').classList.toggle('play-played', newValue === 'false');
-					this.$('#container').classList.toggle('play-unplayed', newValue === null);
-					break;
-			}
+			this.$('#container').classList.toggle('selected', value === 'true'); // for element.css
+			this.$('#container').classList.toggle('play-playing', value === 'true'); // for consistency
+			this.$('#container').classList.toggle('play-played', value === 'false');
+			this.$('#container').classList.toggle('play-unplayed', value === 'undetermined');
 		}
 
 		emitSelect_() {
